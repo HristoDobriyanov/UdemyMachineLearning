d567746d092092781423f4656e5dca28c0b52cdb
Hristo Dobriyanov <hdobriyanov@gmail.com>
2018-11-25 11:10:13 +0200

    Finished Section 5, Lecture 45

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_HRI.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_HRI.py
@@ -16,21 +16,28 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+
+
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
-# Encoding the Dependent Variable
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+
+
+
+# Avoiding the dummy variable trap
+X = X[:, 1:]
+
 
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -39,4 +46,45 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+
+# Fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
+
+# Predicting the Test set result
+y_pred = regressor.predict(X_test)
+
+
+
+# Building the optimal model usig Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
